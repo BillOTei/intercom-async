7dccf193086304e28ce80b43eafc4e4734152d91
billotei <ateilhet@gmail.com>
2016-02-19 12:44:31 +0100

    basic to reconstruct the ByteString flow and treat it

--- app/Server.scala
+++ app/Server.scala
@@ -15,7 +15,16 @@ object Server {
 
     val handler = Sink.foreach[Tcp.IncomingConnection] { conn =>
       Logger.info("Event server connected to: " + conn.remoteAddress)
-      conn handleWith Flow[ByteString]
+      // Get the ByteString flow and reconstruct the msg for handling and then output it back
+      // that is how handleWith work apparently
+      conn.handleWith(
+        Flow[ByteString].fold(ByteString.empty)((acc, b) => acc ++ b).map(
+          b => {
+            Logger.info("Event server received message: " + b.utf8String)
+            b.utf8String
+          }
+        ).map(ByteString(_))
+      )
     }
 
     val connections = Tcp().bind(address, port)
