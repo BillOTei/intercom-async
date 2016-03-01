6b5a2bbb74eca3c0c4338da563a62792d9eb46d4
billotei <ateilhet@gmail.com>
2016-03-02 00:45:15 +0100

    company primary levels data

--- app/models/intercom/Company.scala
+++ app/models/intercom/Company.scala
@@ -1,7 +1,8 @@
 package models.intercom
 
 import io.intercom.api.{Company => IntercomCompany, CustomAttribute}
-import models.centralapp.Place
+import models.centralapp.{Category, Place}
+import org.joda.time.DateTime
 
 import scala.util.Try
 
@@ -13,30 +14,36 @@ object Company {
     * @return
     */
   def getBasicIntercomCompany(company: Place): IntercomCompany = new IntercomCompany().
-    setCompanyID("centralapp-" + company.centralAppId.toString).
-    addCustomAttribute(CustomAttribute.newLongAttribute("central_app_id", company.centralAppId)).
-    setName(company.name).
-    addCustomAttribute(CustomAttribute.newStringAttribute("email", company.email.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newBooleanAttribute("in_chain", company.inChain)).
-    addCustomAttribute(CustomAttribute.newStringAttribute("chain_name", company.chainName.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("country_code", company.countryCode)).
-    addCustomAttribute(CustomAttribute.newStringAttribute("locality", company.locality)).
-    addCustomAttribute(CustomAttribute.newStringAttribute("zip", company.zip)).
-    addCustomAttribute(CustomAttribute.newStringAttribute("address", company.address)).
-    addCustomAttribute(CustomAttribute.newStringAttribute("default_lang", company.defaultLang.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("opening_dates", company.openingDates.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("landline_phone", company.landlinePhone.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("mobile_phone", company.mobilePhone.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("website", company.website.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("primary_cat_first_lvl", company.primaryCatFirstLvl.getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("primary_cat_last_lvl", company.primaryCatLastLvl.getOrElse(""))).
-    setRemoteCreatedAt(company.signupDate / 1000).
-    addCustomAttribute(CustomAttribute.newBooleanAttribute("verification_status", company.verificationStatus)).
-    addCustomAttribute(CustomAttribute.newDoubleAttribute("completion_score", company.completionScore)).
-    addCustomAttribute(CustomAttribute.newIntegerAttribute("nb_of_actions_to_take", company.nbOfActionsToTake.getOrElse(0))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("distrib_name", company.attribution.flatMap(_.distribName).getOrElse(""))).
-    addCustomAttribute(CustomAttribute.newLongAttribute("creatorCentralAppId", company.attribution.flatMap(_.creatorCentralAppId).getOrElse(0))).
-    addCustomAttribute(CustomAttribute.newStringAttribute("creatorEmail", company.attribution.flatMap(_.creatorEmail).getOrElse("")))
+      setCompanyID("centralapp-" + company.centralAppId.toString).
+      addCustomAttribute(CustomAttribute.newLongAttribute("place_id", company.centralAppId)).
+      setName(company.name).
+      addCustomAttribute(CustomAttribute.newStringAttribute("place_email", company.email.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newBooleanAttribute("in_chain", company.chainName.isDefined)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("chain_name", company.chainName.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("country_code", company.countryCode)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("locality", company.locality)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("zip_code", company.zip)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("full_address", company.address)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("default_language", company.defaultLang.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("opening_date", company.openingDates.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("primary_phone", company.landlinePhone.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("mobile_phone", company.mobilePhone.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("website", company.website.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute(
+        "primary_cat_first_lvl",
+        Category.getPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
+      )).
+      addCustomAttribute(CustomAttribute.newStringAttribute(
+        "primary_cat_last_lvl",
+        Category.getLastPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
+      )).
+      setRemoteCreatedAt(DateTime.parse(company.signupDate).getMillis * 1000).
+      addCustomAttribute(CustomAttribute.newBooleanAttribute("verification_status", company.verificationStatus)).
+      addCustomAttribute(CustomAttribute.newDoubleAttribute("completion_score", company.completionScore)).
+      addCustomAttribute(CustomAttribute.newIntegerAttribute("nb_of_actions_to_take", company.nbOfActionsToTake.getOrElse(0))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("distributor_name", company.attribution.flatMap(_.distribName).getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newLongAttribute("owner_user_id", company.attribution.flatMap(_.creatorCentralAppId).getOrElse(0))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("owner_user_email", company.attribution.flatMap(_.creatorEmail).getOrElse("")))
 
   /**
     * Create a company at Intercom
