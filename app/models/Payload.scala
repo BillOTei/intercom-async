93f0e1ff8bd0e8e8e73883e7eb4f70ed614945db
billotei <ateilhet@gmail.com>
2016-03-01 11:28:39 +0100

    not used anymore, see if necessary according to specs

--- app/models/Payload.scala
+++ app/models/Payload.scala
@@ -1,6 +1,6 @@
 package models
 
-import models.intercom.{Company, User}
+import models.centralapp.{Place, User}
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
 import play.api.libs.json._
@@ -8,12 +8,12 @@ import play.api.libs.json._
 /**
   * Created by BillOTei on 17/02/16
   */
-case class Payload(user: Option[User], place: Option[Company]/*, event: Option[Event]*/)
+case class Payload(user: Option[User], place: Option[Place]/*, event: Option[Event]*/)
 
 object Payload {
   implicit val payloadReads: Reads[Payload] = (
     (JsPath \ "user").readNullable[User] and
-    (JsPath \ "place").readNullable[Company] /*and
+    (JsPath \ "place").readNullable[Place] /*and
     (JsPath \ "event").readNullable[Event]*/
     )(Payload.apply _)
 }
