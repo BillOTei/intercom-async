eccb08a8e4d17d9ed5b03a53b5fbec5399376cd8
billotei <ateilhet@gmail.com>
2016-04-21 10:29:37 +0200

    init conversation to intercom

--- app/models/intercom/ConversationInit.scala
+++ app/models/intercom/ConversationInit.scala
@@ -12,10 +12,10 @@ object ConversationInit {
   implicit val jsonWrites: Writes[ConversationInit] = new Writes[ConversationInit] {
     override def writes(o: ConversationInit): JsValue =
       Json.obj(
-        "from" -> Json.obj {
+        "from" -> Json.obj (
           "type" -> "user",
           "id" -> "536e564f316c83104c000020"
-        },
+        ),
         "body" -> "Hey"
       )
   }
