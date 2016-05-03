35c0a5af402225324c5aa6faef916aec681fd00d
billotei <ateilhet@gmail.com>
2016-05-03 11:23:48 +0200

    no akka streams server startup atm

--- app/Global.scala
+++ app/Global.scala
@@ -1,15 +1,13 @@
-import akka.actor.ActorSystem
 import play.api.{GlobalSettings, Logger}
-import service.Server
 
 /**
  * Created by BillOTei on 17/02/16
  */
 object Global extends GlobalSettings {
-  override def onStart(app: play.api.Application): Unit = {
+  // No stream anymore, http prefered
+  /*override def onStart(app: play.api.Application): Unit = {
     Logger.info("Centralapp Events Service starting")
 
-    // No stream anymore, http prefered
-    //Server.connectStream(ActorSystem("CentralappEvents"), "localhost", 2554)
-  }
+    Server.connectStream(ActorSystem("CentralappEvents"), "localhost", 2554)
+  }*/
 }
