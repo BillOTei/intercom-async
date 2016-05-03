bc4b21cd29975046e2ade490f24e737cccbd3cdf
billotei <ateilhet@gmail.com>
2016-05-03 11:45:06 +0200

    event action refactoring

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,11 +1,14 @@
 package controllers
 
+import helpers.JsonError
 import models.Message
 import play.api.mvc._
 import play.libs.Akka
 import service.actors.ForwardActor
 import service.actors.ForwardActor.Forward
+
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class Application extends Controller {
   val system = Akka.system()
@@ -14,17 +17,18 @@ class Application extends Controller {
     Ok("Index Centralapp Events Service")
   }
 
-  def event = Action.async {
-    implicit request => scala.concurrent.Future(
-      {
-        for {
-          json <- request.body.asJson
-          msg <- json.validate(Message.messageReads).asOpt
-        } yield {
+  /**
+    * Endpoint to receive messages as http
+    * @return
+    */
+  def event = Action.async(parse.json) {
+    implicit request =>
+      request.body.validate[Message[Nothing]].map {
+        case msg: Message[Nothing] =>
           system.actorOf(ForwardActor.props) ! Forward(msg)
-          Ok
-        }
-      } getOrElse BadRequest("MESSAGE.INVALID")
-    )
+          Future(Ok)
+      }.recoverTotal {
+        e => Future(BadRequest(JsonError.jsErrors(e)))
+      }
   }
 }
