0c9ffd8b85112fd18ef04ff891924eb7ecb059fb
billotei <ateilhet@gmail.com>
2016-05-03 17:03:22 +0200

    action composition with user authentication

---
+++ app/controllers/actions/UserActions.scala
@@ -0,0 +1,43 @@
+package controllers.actions
+
+import helpers.WSResponseExtender.WSResponseMapper
+import helpers.{HttpClient, JsonError}
+import models.centralapp.contacts.UserContact
+import models.centralapp.users.User
+import play.api.Play._
+import play.api.mvc._
+
+import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
+
+object UserActions {
+
+  object authenticatedAction extends ActionBuilder[AuthenticatedRequest] {
+    override def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[Result]): Future[Result] = {
+      {
+        request.getQueryString("token") orElse {
+          Try {
+            request.body.asInstanceOf[AnyContentAsJson]
+          }.toOption flatMap {
+            _.asJson
+          } flatMap {
+            json => (json \ "token").asOpt[String]
+          }
+        } flatMap {
+          token =>
+            HttpClient.get(current.configuration.getString("coreservice.user.url").getOrElse(""), "token" -> token).toOption map {
+              f => f flatMap {
+                resp => resp.mapToResult[User](None)(User.userReads) match {
+                  case Success(user) => block(new AuthenticatedRequest[A](user, request))
+                  case Failure(err) => Future.successful(Results.Unauthorized(JsonError.stringError(UserContact.MSG_UNAUTHORIZED)))
+                }
+              }
+            }
+        }
+      } getOrElse Future.successful(Results.Unauthorized(JsonError.stringError(UserContact.MSG_UNAUTHORIZED)))
+    }
+  }
+
+  class AuthenticatedRequest[A](val user: User, request: Request[A]) extends WrappedRequest[A](request)
+
+}
