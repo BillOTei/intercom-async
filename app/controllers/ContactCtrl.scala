375e12458a226250ba90437b9064c5cc00c567f5
billotei <ateilhet@gmail.com>
2016-05-09 13:06:30 +0200

    user contact doc

--- app/controllers/ContactCtrl.scala
+++ app/controllers/ContactCtrl.scala
@@ -2,6 +2,7 @@ package controllers
 
 import controllers.actions.UserActions.authenticatedAction
 import helpers.JsonError
+import io.swagger.annotations.{ApiResponse, _}
 import models.centralapp.contacts.{LeadContact, UserContact}
 import play.api.mvc._
 import play.libs.Akka
@@ -9,6 +10,7 @@ import play.libs.Akka
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
+@Api(value = "/contact")
 class ContactCtrl extends Controller {
   val system = Akka.system()
 
@@ -17,7 +19,6 @@ class ContactCtrl extends Controller {
     *
     * @return
     */
-  // Todo add checks about leads/users that are already
   /*
   Donc scénario 1 :
   Quand un mec s’inscrit —> Si jamais c’était déjà un lead ave qui tu as conversé , etc. Et il arrive en new user out of no-where baaaaahhh tu vas lui envoyer des message discordant —> Donc il faut matcher le lead correspondant
@@ -50,6 +51,36 @@ class ContactCtrl extends Controller {
   [1:00]
   il aura l’air con… puis il va devoir retrouver les convers, etc.
   */
+  @ApiOperation(
+    value = "Endpoint to receive a contact request from authenticated user",
+    notes = "Only auth users with core token can use this endpoint. Logic heavily relies on Intercom conversations endpoints" +
+      " as it is the only service used atm. Users are upserted " +
+      "and companies are created if name and location parameters are present. " +
+      "Conversation is initiated only if message or when to contact are present. Tag is created on the user" +
+      "with subject.",
+    response = classOf[Result],
+    httpMethod = "POST",
+    nickname = "user contact forwarding endpoint"
+  )
+  @ApiImplicitParams(
+    Array(
+      new ApiImplicitParam(name = "user_id", value = "Centralapp user_id", required = true, dataType = "int", paramType = "body"),
+      new ApiImplicitParam(name = "token", value = "Centralapp user token query string or body", dataType = "string", paramType = "query"),
+      new ApiImplicitParam(name = "token", value = "Centralapp user token query string or body", dataType = "string", paramType = "body"),
+      new ApiImplicitParam(name = "subject", value = "Message subject", required = true, dataType = "String", paramType = "body"),
+      new ApiImplicitParam(name = "message", value = "Optional message body", dataType = "String", paramType = "body"),
+      new ApiImplicitParam(name = "when_to_contact", value = "Optional user inputed best time to be contacted", dataType = "String", paramType = "body"),
+      new ApiImplicitParam(name = "business_name", value = "The optional user provided company name", dataType = "String", paramType = "body"),
+      new ApiImplicitParam(name = "location", value = "The optional user provided company location", dataType = "String", paramType = "body")
+    )
+  )
+  @ApiResponses(
+    Array(
+      new ApiResponse(code = 202, message = ""),
+      new ApiResponse(code = 400, message = "Multiple possible formatted msgs, cf core json parsing doc."),
+      new ApiResponse(code = 401, message = "ERR.USER.UNAUTHORIZED")
+    )
+  )
   def userContact = authenticatedAction.async(parse.json) {
     implicit request =>
       request.body.validate[UserContact].map {
@@ -58,7 +89,7 @@ class ContactCtrl extends Controller {
 
             UserContact.process(uc, request.user.email)
             Future(Accepted)
-          } else Future(BadRequest(JsonError.stringError(UserContact.MSG_UNAUTHORIZED)))
+          } else Future(Unauthorized(JsonError.stringError(UserContact.MSG_UNAUTHORIZED)))
       }.recoverTotal {
         e => Future(BadRequest(JsonError.jsErrors(e)))
       }
