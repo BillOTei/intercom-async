6bded731dc945ed3e46e292f23bf50ca18d53ca4
billotei <ateilhet@gmail.com>
2016-06-30 16:53:36 +0200

    refactoring place reads for field addition

--- app/models/intercom/Company.scala
+++ app/models/intercom/Company.scala
@@ -18,37 +18,36 @@ object Company {
     * @return
     */
   def getBasicIntercomCompany(company: Place): IntercomCompany = new IntercomCompany().
-      setCompanyID(company.centralAppId.toString).
-      addCustomAttribute(CustomAttribute.newLongAttribute("place_id", company.centralAppId)).
-      setName(company.name).
-      addCustomAttribute(CustomAttribute.newStringAttribute("place_email", company.email.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newBooleanAttribute("in_chain", company.chainName.isDefined)).
-      addCustomAttribute(CustomAttribute.newStringAttribute("chain_name", company.chainName.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("country_code", company.countryCode.getOrElse("not_specified"))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("locality", company.locality.getOrElse("not_specified"))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("zip_code", company.zip.getOrElse("not_specified"))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("full_address", company.address + " " + company.streetNumber)).
-      addCustomAttribute(CustomAttribute.newStringAttribute("default_language", company.defaultLang.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("opening_date", company.openingDates.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("primary_phone", company.landlinePhone.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("mobile_phone", company.mobilePhone.getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("website", company.website.getOrElse(""))).
+      setCompanyID(company.placePart1.centralAppId.toString).
+      addCustomAttribute(CustomAttribute.newLongAttribute("place_id", company.placePart1.centralAppId)).
+      setName(company.placePart1.name).
+      addCustomAttribute(CustomAttribute.newStringAttribute("place_email", company.placePart1.email.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newBooleanAttribute("in_chain", company.placePart1.chainName.isDefined)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("chain_name", company.placePart1.chainName.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("country_code", company.placePart1.countryCode.getOrElse("not_specified"))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("locality", company.placePart1.locality.getOrElse("not_specified"))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("zip_code", company.placePart1.zip.getOrElse("not_specified"))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("full_address", company.placePart1.address + " " + company.placePart1.streetNumber)).
+      addCustomAttribute(CustomAttribute.newStringAttribute("default_language", company.placePart1.defaultLang.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("opening_date", company.placePart1.openingDates.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("primary_phone", company.placePart1.landlinePhone.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("mobile_phone", company.placePart1.mobilePhone.getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("website", company.placePart2.website.getOrElse(""))).
       addCustomAttribute(CustomAttribute.newStringAttribute(
         "primary_cat_last_lvl",
-        Category.getPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
+        Category.getPrimaryOpt(company.placePart2.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
       )).
       addCustomAttribute(CustomAttribute.newStringAttribute(
         "primary_cat_first_lvl",
-        Category.getLastPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
+        Category.getLastPrimaryOpt(company.placePart2.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")
       )).
-      setRemoteCreatedAt(company.signupDate.map(DateTime.parse(_).getMillis / 1000).getOrElse(0)).
-      addCustomAttribute(CustomAttribute.newBooleanAttribute("verification_status", company.verificationStatus)).
-      addCustomAttribute(CustomAttribute.newDoubleAttribute("completion_score", company.completionScore)).
-      addCustomAttribute(CustomAttribute.newIntegerAttribute("nb_of_actions_to_take", company.nbOfActionsToTake.getOrElse(0))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("distributor_name", company.attribution.flatMap(_.distribName).getOrElse(""))).
-      addCustomAttribute(CustomAttribute.newLongAttribute("owner_user_id", company.attribution.flatMap(_.creatorCentralAppId).getOrElse(0))).
-      addCustomAttribute(CustomAttribute.newStringAttribute("owner_user_email", company.attribution.flatMap(_.creatorEmail).getOrElse(""))).
-      setPlan(new IntercomCompany.Plan(company.plan.map(_.name).getOrElse("")))
+      setRemoteCreatedAt(company.placePart2.signupDate.map(DateTime.parse(_).getMillis / 1000).getOrElse(0)).
+      addCustomAttribute(CustomAttribute.newBooleanAttribute("verification_status", company.placePart2.verificationStatus)).
+      addCustomAttribute(CustomAttribute.newDoubleAttribute("completion_score", company.placePart2.completionScore)).
+      addCustomAttribute(CustomAttribute.newIntegerAttribute("nb_of_actions_to_take", company.placePart2.nbOfActionsToTake.getOrElse(0))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("distributor_name", company.placePart2.attribution.flatMap(_.distribName).getOrElse(""))).
+      addCustomAttribute(CustomAttribute.newLongAttribute("owner_user_id", company.placePart2.attribution.flatMap(_.creatorCentralAppId).getOrElse(0))).
+      addCustomAttribute(CustomAttribute.newStringAttribute("owner_user_email", company.placePart2.attribution.flatMap(_.creatorEmail).getOrElse("")))
 
   /**
     * Create a company at Intercom
@@ -66,11 +65,11 @@ object Company {
     * @return
     */
   def isValid(company: Place): Boolean = {
-    company.centralAppId > 0 && !company.name.isEmpty && (company.email.isEmpty ||
-      """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r.unapplySeq(company.email.get).isDefined)
+    company.placePart1.centralAppId > 0 && !company.placePart1.name.isEmpty && (company.placePart1.email.isEmpty ||
+      """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r.unapplySeq(company.placePart1.email.get).isDefined)
     /*&&
-    company.attribution.creatorCentralAppId.isDefined &&
-    (company.attribution.creatorEmail.isEmpty || """([\w\.]+)@([\w\.]+)""".r.unapplySeq(company.attribution.creatorEmail.get).isDefined)*/
+    company.placePart2.attribution.creatorCentralAppId.isDefined &&
+    (company.placePart2.attribution.creatorEmail.isEmpty || """([\w\.]+)@([\w\.]+)""".r.unapplySeq(company.placePart2.attribution.creatorEmail.get).isDefined)*/
   }
 
   /**
@@ -82,54 +81,54 @@ object Company {
     */
   def toJson(company: Place, remove: Boolean = false) = {
     val customAttributes = Json.obj(
-      "place_id" -> company.centralAppId,
-      "place_email" -> company.email,
-      "in_chain" -> company.chainName.isDefined,
-      "chain_name" -> JsString(company.chainName.getOrElse("")),
-      "default_language" -> JsString(company.defaultLang.getOrElse("")),
-      "primary_phone" -> JsString(company.landlinePhone.getOrElse("")),
-      "mobile_phone" -> JsString(company.mobilePhone.getOrElse("")),
-      "website" -> JsString(company.website.getOrElse("")),
-      "primary_cat_last_lvl" -> JsString(Category.getPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")),
-      "primary_cat_first_lvl" -> JsString(Category.getLastPrimaryOpt(company.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")),
-      "verification_status" -> company.verificationStatus,
-      "completion_score" -> company.completionScore,
-      "nb_of_actions_to_take" -> Json.toJson(company.nbOfActionsToTake.getOrElse(0)),
-      "distributor_name" -> JsString(company.attribution.flatMap(_.distribName).getOrElse("")),
-      "lead" -> company.lead
+      "place_id" -> company.placePart1.centralAppId,
+      "place_email" -> company.placePart1.email,
+      "in_chain" -> company.placePart1.chainName.isDefined,
+      "chain_name" -> JsString(company.placePart1.chainName.getOrElse("")),
+      "default_language" -> JsString(company.placePart1.defaultLang.getOrElse("")),
+      "primary_phone" -> JsString(company.placePart1.landlinePhone.getOrElse("")),
+      "mobile_phone" -> JsString(company.placePart1.mobilePhone.getOrElse("")),
+      "website" -> JsString(company.placePart2.website.getOrElse("")),
+      "primary_cat_last_lvl" -> JsString(Category.getPrimaryOpt(company.placePart2.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")),
+      "primary_cat_first_lvl" -> JsString(Category.getLastPrimaryOpt(company.placePart2.categories.get).flatMap(c => (c \ "name").asOpt[String]).getOrElse("")),
+      "verification_status" -> company.placePart2.verificationStatus,
+      "completion_score" -> company.placePart2.completionScore,
+      "nb_of_actions_to_take" -> Json.toJson(company.placePart2.nbOfActionsToTake.getOrElse(0)),
+      "distributor_name" -> JsString(company.placePart2.attribution.flatMap(_.distribName).getOrElse("")),
+      "lead" -> false
     ) ++ {
-      if (company.openingDates.isDefined) Json.obj("opening_date" -> DateTime.parse(company.openingDates.get).getMillis / 1000)
+      if (company.placePart1.openingDates.isDefined) Json.obj("opening_date" -> DateTime.parse(company.placePart1.openingDates.get).getMillis / 1000)
       else Json.obj()
     } ++ {
-      if (company.locality.isDefined) {
+      if (company.placePart1.locality.isDefined) {
         Json.obj(
-          "country_code" -> JsString(company.countryCode.getOrElse("not_specified")),
-          "locality" -> company.locality.get,
-          "zip_code" -> JsString(company.zip.getOrElse("not_specified")),
-          "full_address" -> JsString(company.address.getOrElse("not_specified") + " " + company.streetNumber.getOrElse("not_specified"))
+          "country_code" -> JsString(company.placePart1.countryCode.getOrElse("not_specified")),
+          "locality" -> company.placePart1.locality.get,
+          "zip_code" -> JsString(company.placePart1.zip.getOrElse("not_specified")),
+          "full_address" -> JsString(company.placePart1.address.getOrElse("not_specified") + " " + company.placePart1.streetNumber.getOrElse("not_specified"))
         )
       } else Json.obj()
     }
 
     /*++ {
-      if (company.attribution.exists(_.creatorCentralAppId.isDefined)) {
+      if (company.placePart2.attribution.exists(_.creatorCentralAppId.isDefined)) {
         Json.obj(
-          "owner_user_id" -> company.attribution.flatMap(_.creatorCentralAppId).get.toString,
-          "owner_user_email" -> JsString(company.attribution.flatMap(_.creatorEmail).getOrElse(""))
+          "owner_user_id" -> company.placePart2.attribution.flatMap(_.creatorCentralAppId).get.toString,
+          "owner_user_email" -> JsString(company.placePart2.attribution.flatMap(_.creatorEmail).getOrElse(""))
         )
       } else Json.obj()
     }*/
 
     Json.obj(
-      "name" -> company.name,
-      "company_id" -> JsString(company.centralAppId.toString),
+      "name" -> company.placePart1.name,
+      "company_id" -> JsString(company.placePart1.centralAppId.toString),
       "custom_attributes" -> customAttributes,
-      "plan" -> JsString(company.plan.map(_.name).getOrElse(""))
+      "plan" -> JsString(company.placePart2.plan.map(_.name).getOrElse(""))
     ) ++ {
-      if (company.signupDate.isDefined) {
+      if (company.placePart2.signupDate.isDefined) {
         Json.obj(
-          "remote_created_at" -> DateTime.parse(company.signupDate.get).getMillis / 1000,
-          "created_at" -> DateTime.parse(company.signupDate.get).getMillis / 1000
+          "remote_created_at" -> DateTime.parse(company.placePart2.signupDate.get).getMillis / 1000,
+          "created_at" -> DateTime.parse(company.placePart2.signupDate.get).getMillis / 1000
         )
       } else {
         Json.obj()
